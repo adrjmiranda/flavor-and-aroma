249e46016ed40955fa318c81abcef1e1dfb0bf3b
Adriano Miranda <adrjmiranda@gmail.com>
2024-08-04 23:18:48 -0300

    chore: added fictitious user to database

---
+++ App/Migrations/Version20240805021758.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240805021758 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE comments CHANGE content content TEXT NOT NULL');
+        $this->addSql('ALTER TABLE posts CHANGE body body LONGTEXT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE comments CHANGE content content TEXT NOT NULL');
+        $this->addSql('ALTER TABLE posts CHANGE body body LONGTEXT NOT NULL');
+    }
+}
